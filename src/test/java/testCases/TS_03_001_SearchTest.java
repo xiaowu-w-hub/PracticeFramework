7ef7e7bfd46fe7521ee6ae784b08dbf877a0409e
xiaowu <guozhixia2022@gmail.com>
2025-11-13 21:03:19 +0800

    test: 修改test cases TS_03_001

--- src/test/java/testCases/TS_03_001_SearchTest.java
+++ src/test/java/testCases/TS_03_001_SearchTest.java
@@ -2,10 +2,11 @@ package testCases;
 
 import org.testng.annotations.Test;
 
-public class US_03_001_SearchTest {
+public class TS_03_001_SearchTest {
     @Test
     public void testSearch(){
         System.out.println("search function works");
         System.out.println("I added this in GitHub so I can pull the update");
+        System.out.println("the third time to update the search test script");
     }
 }
