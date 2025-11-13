ee506b75a2a17d86bf2624c38129054fe5515b47
xiaowu <guozhixia2022@gmail.com>
2025-11-13 17:51:00 +0800

    feat: 初始化自动化测试项目

---
+++ src/test/java/pageObject/MyAccountPage.java
@@ -0,0 +1,1 @@
+package pageObject;import base.BasePage;import org.openqa.selenium.WebDriver;public class MyAccountPage extends BasePage {    public MyAccountPage(WebDriver driver) {        super(driver);    }}
---
+++ src/test/java/pageObject/SearchPage.java
@@ -0,0 +1,1 @@
+package pageObject;import base.BasePage;import org.openqa.selenium.WebDriver;public class SearchPage extends BasePage {    public SearchPage(WebDriver driver) {        super(driver);    }}
