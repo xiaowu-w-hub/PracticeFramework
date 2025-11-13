8bcada4702b6de53ff693874091a9ba6a8405e5f
xiaowu-w-hub <shanxiwamm@163.com>
2025-11-13 18:32:14 +0800

    Update US_03_001_SearchTest.java
    update the test cases

--- src/test/java/testCases/US_03_001_SearchTest.java
+++ src/test/java/testCases/US_03_001_SearchTest.java
@@ -1,1 +1,11 @@
-package testCases;import org.testng.annotations.Test;public class US_03_001_SearchTest {    @Test    public void testSearch(){        System.out.println("search function works");    }}
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class US_03_001_SearchTest {
+    @Test
+    public void testSearch(){
+        System.out.println("search function works");
+        System.out.println("I added this in GitHub so I can pull the update");
+    }
+}
